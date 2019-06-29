c831f9a73549f24eca5e73beccc7d248461ab7a4
Bin Chen <binchen@Bins-MacBook-Pro-2.local>
2019-06-28 22:52:32 -0400

    Committed coding challenge

--- src/com/company/SingleNumber.java
+++ src/com/company/SingleNumber.java
@@ -1,6 +1,34 @@
 package com.company;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SingleNumber {
+    public static void main(String[] args) {
+        int[] array = {2,2,3};
+        System.out.println(singleNumber(array));
+    }
 
+    public static int singleNumber(int[] nums) {
+        int count = 0;
+        Map<Integer,Integer> countMap = new HashMap<>();
 
+        for(int i = 0; i < nums.length; i++) {
+
+            if(countMap.containsKey(nums[i])) {
+                count = countMap.get(nums[i]);
+                countMap.put(nums[i], ++count);
+            } else {
+                countMap.put(nums[i], 1);
+            }
+        }
+
+        for(Map.Entry<Integer,Integer> entry: countMap.entrySet()) {
+            if(entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+
+        return 0;
+    }
 }
